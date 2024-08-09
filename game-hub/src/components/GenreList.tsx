b501d071ae49978d39225ac593c783527fe45005
Leela-Prasad <leela.jagu@gmail.com>
2024-08-09 15:03:28 +0530

    Display Genres

--- game-hub/src/components/GenreList.tsx
+++ game-hub/src/components/GenreList.tsx
@@ -1,14 +1,21 @@
+import { Text, Image, HStack, List, ListItem } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 function GenreList() {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} marginY="5px">
+          <HStack>
+            <Image src={getCroppedImageUrl(genre.image_background)} width="32px" borderRadius={4} />
+            <Text fontSize={"lg"}>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 }
 
--- game-hub/src/hooks/useGenres.ts
+++ game-hub/src/hooks/useGenres.ts
@@ -3,9 +3,9 @@ import useData from "./useData";
 interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
 
 const useGenres = () => useData<Genre>("/genres");
-
 
 export default useGenres;
